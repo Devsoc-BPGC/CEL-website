87ab688a106c304c9a0227d731bb679e6039edef
Sarang S <iam.sarangs@gmail.com>
2022-08-02 00:11:08 +0530

    fix: revert to styling 0%

--- src/components/TeamMemberGrid.jsx
+++ src/components/TeamMemberGrid.jsx
@@ -1,47 +1,49 @@
 import React from 'react';
 import TeamMemberCard from './TeamMemberCard';
-import { SimpleGrid } from '@chakra-ui/react';
+import { SimpleGrid, Box } from '@chakra-ui/react';
 
 const TeamMemberGrid = () => {
   return (
-    <SimpleGrid columns={[1, 2, 3]} spacing='20px'>
-      <TeamMemberCard
-        name='John Smith'
-        title='Coordinator'
-        contact='+91 1234567890'
-        email='abc@xyz.com'
-      />
-      <TeamMemberCard
-        name='John Smith'
-        title='Coordinator'
-        contact='+91 1234567890'
-        email='abc@xyz.com'
-      />
-      <TeamMemberCard
-        name='John Smith'
-        title='Coordinator'
-        contact='+91 1234567890'
-        email='abc@xyz.com'
-      />
-      <TeamMemberCard
-        name='John Smith'
-        title='Coordinator'
-        contact='+91 1234567890'
-        email='abc@xyz.com'
-      />
-      <TeamMemberCard
-        name='John Smith'
-        title='Coordinator'
-        contact='+91 1234567890'
-        email='abc@xyz.com'
-      />
-      <TeamMemberCard
-        name='John Smith'
-        title='Coordinator'
-        contact='+91 1234567890'
-        email='abc@xyz.com'
-      />
-    </SimpleGrid>
+    <Box textAlign='center' color='white' bg='black' p={100}>
+      <SimpleGrid columns={[1, 2, 3]} spacing='20px' justifyItems='center'>
+        <TeamMemberCard
+          name='John Smith'
+          title='Coordinator'
+          contact='+91 1234567890'
+          email='abc@xyz.com'
+        />
+        <TeamMemberCard
+          name='John Smith'
+          title='Coordinator'
+          contact='+91 1234567890'
+          email='abc@xyz.com'
+        />
+        <TeamMemberCard
+          name='John Smith'
+          title='Coordinator'
+          contact='+91 1234567890'
+          email='abc@xyz.com'
+        />
+        <TeamMemberCard
+          name='John Smith'
+          title='Coordinator'
+          contact='+91 1234567890'
+          email='abc@xyz.com'
+        />
+        <TeamMemberCard
+          name='John Smith'
+          title='Coordinator'
+          contact='+91 1234567890'
+          email='abc@xyz.com'
+        />
+        <TeamMemberCard
+          name='John Smith'
+          title='Coordinator'
+          contact='+91 1234567890'
+          email='abc@xyz.com'
+        />
+      </SimpleGrid>
+    </Box>
   );
 };
 

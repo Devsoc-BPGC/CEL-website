f656a476b11ab60e8cd27cf0725f313f10acee89
Sarang S <iam.sarangs@gmail.com>
2022-08-01 15:12:30 +0530

    feat; add footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,36 @@
+import { Box, Text, VStack, HStack } from '@chakra-ui/react';
+import { PhoneIcon, EmailIcon } from '@chakra-ui/icons';
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <Box bg='black' color='white'>
+      <VStack>
+        <HStack
+          width={'100%'}
+          paddingStart={5}
+          paddingEnd={5}
+          justify={'space-between'}
+          align={'center'}
+        >
+          <Text fontWeight={800} fontSize={'25px'}>
+            CONNECT. INNOVATE. INSPIRE.
+          </Text>
+          <VStack spacing={0} align={'start'}>
+            <Text padding={0} margin={0}>
+              {' '}
+              <PhoneIcon color={'#FDC20A'} /> Contact: +91 1234567890{' '}
+            </Text>
+            <Text padding={0} margin={0}>
+              {' '}
+              <EmailIcon color={'#FDC20A'} /> Email: team@celbitsgoa.com{' '}
+            </Text>
+          </VStack>
+        </HStack>
+        <Text textAlign={'center'}> Made with 💛 by DevSoc</Text>
+      </VStack>
+    </Box>
+  );
+};
+
+export default Footer;

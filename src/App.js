b3a715d06abdfeaae8a31c74e697f1532e6b35b7
Sarang S <iam.sarangs@gmail.com>
2022-08-02 00:04:56 +0530

    fixes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import { ChakraProvider, Box, theme } from '@chakra-ui/react';
+import { ChakraProvider, theme } from '@chakra-ui/react';
 import Navbar from './components/Navbar';
 import TeamMemberGrid from './components/TeamMemberGrid';
 import Footer from './components/Footer';
--- src/components/TeamMemberGrid.jsx
+++ src/components/TeamMemberGrid.jsx
@@ -5,12 +5,42 @@ import { SimpleGrid } from '@chakra-ui/react';
 const TeamMemberGrid = () => {
   return (
     <SimpleGrid columns={[1, 2, 3]} spacing='20px'>
-      <TeamMemberCard />
-      <TeamMemberCard />
-      <TeamMemberCard />
-      <TeamMemberCard />
-      <TeamMemberCard />
-      <TeamMemberCard />
+      <TeamMemberCard
+        name='John Smith'
+        title='Coordinator'
+        contact='+91 1234567890'
+        email='abc@xyz.com'
+      />
+      <TeamMemberCard
+        name='John Smith'
+        title='Coordinator'
+        contact='+91 1234567890'
+        email='abc@xyz.com'
+      />
+      <TeamMemberCard
+        name='John Smith'
+        title='Coordinator'
+        contact='+91 1234567890'
+        email='abc@xyz.com'
+      />
+      <TeamMemberCard
+        name='John Smith'
+        title='Coordinator'
+        contact='+91 1234567890'
+        email='abc@xyz.com'
+      />
+      <TeamMemberCard
+        name='John Smith'
+        title='Coordinator'
+        contact='+91 1234567890'
+        email='abc@xyz.com'
+      />
+      <TeamMemberCard
+        name='John Smith'
+        title='Coordinator'
+        contact='+91 1234567890'
+        email='abc@xyz.com'
+      />
     </SimpleGrid>
   );
 };
